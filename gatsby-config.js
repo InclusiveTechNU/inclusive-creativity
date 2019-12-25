8b0aadcda1687617fb3957f06bb6f662e664b612
Tommy McHugh <tombmch@gmail.com>
2019-12-25 08:44:25 -0500

    initial push with project info

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  siteMetadata: {
+    title: `Accessible Content Production Tools`,
+        description: `Next Generation Content Production Tools for People with Vision Impairments.`,
+    author: `Northwestern Inclusive Technology Lab, Interactive Audio Lab, and Collab Lab`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-image`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+        resolve: 'gatsby-source-prismic-graphql',
+        options: {
+            repositoryName: 'a11y-content-production',
+            linkResolver: () => post => `/${post.uid}`,
+        }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-prismic-starter-prist`,
+        short_name: `prist`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/oscar-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
+    {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+            head: true,
+        },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
